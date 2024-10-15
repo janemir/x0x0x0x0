1e9ea77f6ec253431661b7ae7dbf15634d8b5903
janemir <janemiro2005@gmail.com>
2024-10-15 21:11:54 +0300

    добавила проверку корректности хода\ ввода

--- Program.cs
+++ Program.cs
@@ -38,6 +38,12 @@ class Program
         }
     }
 
+    // Проверка корректности хода (ветка Inputcorrectness)
+    static bool IsValidMove(int row, int col) 
+    {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row, col] == ' ';
+    }
+
     // Основная логика игры
     static void PlayGame() 
     {
@@ -48,6 +54,24 @@ class Program
             PrintBoard(); // Вывод игрового поля
             int row, col;
             bool validMove = false;
+
+            // Проверка корректного ввода (Inputcorrectness)
+            while (!validMove) 
+            {
+                Console.WriteLine($"Ход игрока {currentPlayer}. Введите строку и столбец (0, 1 или 2):");
+                row = int.Parse(Console.ReadLine());
+                col = int.Parse(Console.ReadLine());
+
+                if (IsValidMove(row, col)) 
+                {
+                    board[row, col] = currentPlayer; // Игрок может сделать ход только в пустую клетку
+                    validMove = true;
+                }
+                else 
+                {
+                    Console.WriteLine("Некорректный ход. Попробуйте снова.");
+                }
+            }
         }
     }
 }
